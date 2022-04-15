d1c0d48876a91b98fb44aec6cea186079e62959d
titivat <titivatmo@gmail.com>
2022-04-15 20:21:40 +0700

    question1

---
+++ test/th/ac/kmitl/se/OrderTest.java
@@ -0,0 +1,74 @@
+package th.ac.kmitl.se;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.*;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class OrderTest {
+    OrderDB orderDB;
+    ProductDB productDB;
+    PaymentService paymentService;
+    ShippingService shippingService;
+    Order order;
+
+    @BeforeEach
+    void setUp() {
+        orderDB =  mock(OrderDB.class);
+        productDB =  mock(ProductDB.class);
+        paymentService =  mock(PaymentService.class);
+        shippingService =  mock(ShippingService.class);
+        order = Mockito.spy(new Order(orderDB, productDB, paymentService, shippingService));
+    }
+
+    @Test
+    void testPlacingOrder() {
+        Address mockAddress = new Address("name", "line1", "line2", "district", "city", "postcode");
+        when(orderDB.getOrderID()).thenReturn(123);
+
+        order.place("John", "Appl Watch", 2, mockAddress);
+        assertEquals(Order.Status.PLACED, order.getStatus());
+
+        when(orderDB.retrieveOrder(123)).thenReturn( order );
+        assertEquals( orderDB.retrieveOrder(123), order);
+    }
+
+    @Test
+    void testCancelingUnpaidOrder() {
+
+    }
+
+    @Test
+    void testPaymentSuccess() {
+
+    }
+
+    @Test
+    void testPaymentError() {
+
+    }
+
+    @Test
+    void testPaymentRetrySuccess() {
+
+    }
+
+    @Test
+    void testShippingOrder() {
+
+    }
+
+    @Test
+    void testCancelingPaidOrderSuccess() {
+
+    }
+
+    @Test
+    void testCancelingPaidOrderError() {
+
+    }
+
+}
