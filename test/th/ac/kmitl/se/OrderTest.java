368adbe0040f1353d9811885d53d925c93aca866
titivat <titivatmo@gmail.com>
2022-04-17 01:10:56 +0700

    question3

--- test/th/ac/kmitl/se/OrderTest.java
+++ test/th/ac/kmitl/se/OrderTest.java
@@ -1,8 +1,12 @@
 package th.ac.kmitl.se;
 
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.util.List;
 
@@ -17,10 +21,10 @@ class OrderTest {
 
     @BeforeEach
     void setUp() {
-        orderDB =  mock(OrderDB.class);
-        productDB =  mock(ProductDB.class);
-        paymentService =  mock(PaymentService.class);
-        shippingService =  mock(ShippingService.class);
+        orderDB = mock(OrderDB.class);
+        productDB = mock(ProductDB.class);
+        paymentService = mock(PaymentService.class);
+        shippingService = mock(ShippingService.class);
         order = Mockito.spy(new Order(orderDB, productDB, paymentService, shippingService));
     }
 
@@ -32,8 +36,8 @@ class OrderTest {
         order.place("John", "Appl Watch", 2, mockAddress);
         assertEquals(Order.Status.PLACED, order.getStatus());
 
-        when(orderDB.retrieveOrder(123)).thenReturn( order );
-        assertEquals( orderDB.retrieveOrder(123), order);
+        when(orderDB.retrieveOrder(123)).thenReturn(order);
+        assertEquals(orderDB.retrieveOrder(123), order);
     }
 
     @Test
@@ -47,9 +51,11 @@ class OrderTest {
         order.cancel();
         assertEquals(Order.Status.CANCELED, order.getStatus());
 
-        when(orderDB.retrieveOrder(1)).thenReturn( order );
-        assertEquals( orderDB.retrieveOrder(1), order);
+        when(orderDB.retrieveOrder(1)).thenReturn(order);
+        assertEquals(orderDB.retrieveOrder(1), order);
     }
+
+//    @Captor ArgumentCaptor<PaymentCallback> callbackCaptor;
 
     @Test
     void testPaymentSuccess() {
@@ -58,11 +64,29 @@ class OrderTest {
 
         order.place("John", "Appl Watch", 2, mockAddress);
         assertEquals(Order.Status.PLACED, order.getStatus());
+
+        //payment
+        Card card = new Card("123", "JohnCena", 2, 2023);
+        // pho getTotalCost to work
+        when(productDB.getPrice("Appl Watch")).thenReturn(1500.0F);
+        when(productDB.getWeight("Appl Watch")).thenReturn(350.0F);
+        when(shippingService.getPrice(mockAddress, 700.0F)).thenReturn(50.0F);
+        assertEquals(order.getTotalCost(), 3050.0F);
+
+        //check for Status.PAYMENT_CHECK
+        order.pay(card);
+        assertEquals(Order.Status.PAYMENT_CHECK, order.getStatus());
+
+        ArgumentCaptor<PaymentCallback> callbackCaptor = ArgumentCaptor.forClass(PaymentCallback.class);
+
+        verify(paymentService).pay(any(Card.class), anyFloat(), callbackCaptor.capture());
+        callbackCaptor.getValue().onSuccess("123");
+
+        assertEquals(Order.Status.PAID, order.getStatus());
     }
 
     @Test
     void testPaymentError() {
-        //doAnswer
     }
 
     @Test
