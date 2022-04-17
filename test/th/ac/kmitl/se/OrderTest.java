e13cb743f20dc4d2820dc803d3728ba455df4399
titivat <titivatmo@gmail.com>
2022-04-17 11:34:50 +0700

    fix verify cancel & place order for verify orderDb

--- test/th/ac/kmitl/se/OrderTest.java
+++ test/th/ac/kmitl/se/OrderTest.java
@@ -35,9 +35,7 @@ class OrderTest {
 
         order.place("John", "Appl Watch", 2, mockAddress);
         assertEquals(Order.Status.PLACED, order.getStatus());
-
-        when(orderDB.retrieveOrder(123)).thenReturn(order);
-        assertEquals(orderDB.retrieveOrder(123), order);
+        verify(orderDB).update(order);
     }
 
     @Test
@@ -47,15 +45,12 @@ class OrderTest {
 
         order.place("John", "Appl Watch", 2, mockAddress);
         assertEquals(Order.Status.PLACED, order.getStatus());
+        verify(orderDB).update(order);
 
         order.cancel();
         assertEquals(Order.Status.CANCELED, order.getStatus());
-
-        when(orderDB.retrieveOrder(1)).thenReturn(order);
-        assertEquals(orderDB.retrieveOrder(1), order);
+        verify(orderDB, times(2)).update(order);
     }
-
-//    @Captor ArgumentCaptor<PaymentCallback> callbackCaptor;
 
     @Test
     void testPaymentSuccess() {
@@ -87,11 +82,56 @@ class OrderTest {
 
     @Test
     void testPaymentError() {
+        Address mockAddress = new Address("name", "line1", "line2", "district", "city", "postcode");
+        when(orderDB.getOrderID()).thenReturn(1);
+
+        order.place("John", "Appl Watch", 2, mockAddress);
+        assertEquals(Order.Status.PLACED, order.getStatus());
+
+        //payment
+        Card card = new Card("123", "JohnCena", 2, 2023);
+        // pho getTotalCost to work
+        when(productDB.getPrice("Appl Watch")).thenReturn(1500.0F);
+        when(productDB.getWeight("Appl Watch")).thenReturn(350.0F);
+        when(shippingService.getPrice(mockAddress, 700.0F)).thenReturn(50.0F);
+        assertEquals(order.getTotalCost(), 3050.0F);
+
+        verify(orderDB).update(order);
+        //check for Status.PAYMENT_ERROR
+        order.pay(card);
+        assertEquals(Order.Status.PAYMENT_CHECK, order.getStatus());
+
+        ArgumentCaptor<PaymentCallback> callbackCaptor = ArgumentCaptor.forClass(PaymentCallback.class);
+
+        verify(paymentService).pay(any(Card.class), anyFloat(), callbackCaptor.capture());
+        callbackCaptor.getValue().onError("123");
+
+        assertEquals(Order.Status.PAYMENT_ERROR, order.getStatus());
     }
 
     @Test
     void testPaymentRetrySuccess() {
+        float price = 1500f;
+        float weight = 350f;
+        int quantity = 2;
+        String productID = "Apple Watch";
+        String successCode = "1234";
+        Address mockAddress = new Address("name", "line1", "line2", "district", "city", "postcode");
+        Card card = new Card("123", "JohnCena", 2, 2023);
 
+        when(orderDB.getOrderID()).thenReturn(1);
+        when(productDB.getPrice(productID)).thenReturn(price);
+        when(productDB.getWeight(productID)).thenReturn(weight);
+        when(shippingService.getPrice(mockAddress,weight*quantity)).thenReturn(50f);
+        order.place("John", productID,quantity, mockAddress);
+        assertEquals(Order.Status.PLACED, order.getStatus());
+        verify(orderDB, times(1)).update(order);
+        //Execute without callback
+        order.pay(card);
+        //Verify
+        assertEquals(Order.Status.PAYMENT_CHECK, order.getStatus());
+        assertEquals(3050f, order.getTotalCost());
+        verify(orderDB, times(2)).update(order);
     }
 
     @Test
